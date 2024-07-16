d493479904d0b6362e4e2bd0dd1e667bd3b9dd50
vishnu <adenavishnu1@gmail.com>
2024-07-16 08:12:12 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,5 +3,8 @@
 # Substitute environment variables in the NGINX configuration template
 envsubst '\$FRONTEND_URL \$API_GATEWAY_URL \$EUREKA_URL' < /etc/nginx/nginx.template.conf > /etc/nginx/nginx.conf
 
+# Display the final configuration for debugging
+cat /etc/nginx/nginx.conf
+
 # Start NGINX
 nginx -g 'daemon off;'
